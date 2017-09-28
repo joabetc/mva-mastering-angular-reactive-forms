4ea25562743254ed12a3ebe2e6ada973bae6e56f
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-09-28 17:41:52 -0300

    Collecting Multiple Addresses

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, FormArray } from "@angular/forms";
 
 @Component({
     selector: "main",
@@ -15,29 +15,32 @@ import { FormControl, FormGroup } from "@angular/forms";
             <input type="text" id="last-name-input" formControlName="lastNameInput">
             <br>Value: {{profileForm.controls['lastNameInput'].value}}
         </div>
-        <div formGroupName="addressGroup">
-            <h2>Address</h2>
-            <div>
-                <label for="street-input">Street:</label>
-                <input type="text" id="street-input" formControlName="streetInput">
-                <br>Value: {{profileForm.controls['addressGroup'].controls['streetInput'].value}}
-            </div>
-            <div>
-                <label for="city-input">City:</label>
-                <input type="text" id="city-input" formControlName="cityInput">
-                <br>Value: {{profileForm.controls['addressGroup'].controls['cityInput'].value}}
-            </div>
-            <div>
-                <label for="state-input">State:</label>
-                <input type="text" id="state-input" formControlName="stateInput">
-                <br>Value: {{profileForm.controls['addressGroup'].controls['stateInput'].value}}
-            </div>
-            <div>
-                <label for="zip-code-input">Zip Code:</label>
-                <input type="text" id="zip-code-input" formControlName="zipCodeInput">
-                <br>Value: {{profileForm.controls['addressGroup'].controls['zipCodeInput'].value}}
+        <div formArrayName="addressGroups">
+            <div *ngFor="let addressGroup of profileForm.controls['addressGroups'].controls" [formGroup]="addressGroup">
+                <h2>Address</h2>
+                <div>
+                    <label for="street-input">Street:</label>
+                    <input type="text" id="street-input" formControlName="streetInput">
+                    <br>Value: {{addressGroup.controls['streetInput'].value}}
+                </div>
+                <div>
+                    <label for="city-input">City:</label>
+                    <input type="text" id="city-input" formControlName="cityInput">
+                    <br>Value: {{addressGroup.controls['cityInput'].value}}
+                </div>
+                <div>
+                    <label for="state-input">State:</label>
+                    <input type="text" id="state-input" formControlName="stateInput">
+                    <br>Value: {{addressGroup.controls['stateInput'].value}}
+                </div>
+                <div>
+                    <label for="zip-code-input">Zip Code:</label>
+                    <input type="text" id="zip-code-input" formControlName="zipCodeInput">
+                    <br>Value: {{addressGroup.controls['zipCodeInput'].value}}
+                </div>
             </div>
         </div>
+        <button type="button" (click)="addAddressGroup()">Add Address</button>
     </form>
     `,
 })
@@ -46,14 +49,23 @@ export class AppComponent {
     public profileForm: FormGroup = new FormGroup({
         firstNameInput: new FormControl(""),
         lastNameInput: new FormControl(""),
-        addressGroup: new FormGroup({
+        addressGroups: new FormArray([
+            this.newAddressGroup(),
+        ]),
+    });
+    
+    public addAddressGroup() {
+        const fa = this.profileForm.controls["addressGroups"] as FormArray;
+        
+        fa.push(this.newAddressGroup());
+    }
+    
+    private newAddressGroup() {
+        return new FormGroup({
             streetInput: new FormControl(""),
             cityInput: new FormControl(""),
             stateInput: new FormControl(""),
             zipCodeInput: new FormControl(""),
-        }),
-    });
-
-    public message: string = "Hello World!";
-
+        })
+    }
 }
