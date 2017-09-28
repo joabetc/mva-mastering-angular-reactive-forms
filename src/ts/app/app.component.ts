ca7d5967247f8f85471a3dbc97805925ab382d57
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-09-28 17:57:33 -0300

    Using Form Builder to Create the Profile Form and Displaying Collected Profile Data

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormControl, FormGroup, FormArray } from "@angular/forms";
+import { FormControl, FormGroup, FormArray, FormBuilder } from "@angular/forms";
 
 @Component({
     selector: "main",
@@ -8,12 +8,10 @@ import { FormControl, FormGroup, FormArray } from "@angular/forms";
         <div>
             <label for="first-name-input">First Name:</label>
             <input type="text" id="first-name-input" formControlName="firstNameInput">
-            <br>Value: {{profileForm.controls['firstNameInput'].value}}
         </div>
         <div>
             <label for="last-name-input">Last Name:</label>
             <input type="text" id="last-name-input" formControlName="lastNameInput">
-            <br>Value: {{profileForm.controls['lastNameInput'].value}}
         </div>
         <div formArrayName="addressGroups">
             <div *ngFor="let addressGroup of profileForm.controls['addressGroups'].controls" [formGroup]="addressGroup">
@@ -21,43 +19,56 @@ import { FormControl, FormGroup, FormArray } from "@angular/forms";
                 <div>
                     <label for="street-input">Street:</label>
                     <input type="text" id="street-input" formControlName="streetInput">
-                    <br>Value: {{addressGroup.controls['streetInput'].value}}
                 </div>
                 <div>
                     <label for="city-input">City:</label>
                     <input type="text" id="city-input" formControlName="cityInput">
-                    <br>Value: {{addressGroup.controls['cityInput'].value}}
                 </div>
                 <div>
                     <label for="state-input">State:</label>
                     <input type="text" id="state-input" formControlName="stateInput">
-                    <br>Value: {{addressGroup.controls['stateInput'].value}}
                 </div>
                 <div>
                     <label for="zip-code-input">Zip Code:</label>
                     <input type="text" id="zip-code-input" formControlName="zipCodeInput">
-                    <br>Value: {{addressGroup.controls['zipCodeInput'].value}}
                 </div>
             </div>
         </div>
         <button type="button" (click)="addAddressGroup()">Add Address</button>
+
+        <div>
+            <button type="button" (click)="saveProfileForm()">Save Profile</button>
+        </div>
     </form>
     `,
 })
 export class AppComponent {
 
-    public profileForm: FormGroup = new FormGroup({
-        firstNameInput: new FormControl(""),
-        lastNameInput: new FormControl(""),
-        addressGroups: new FormArray([
-            this.newAddressGroup(),
-        ]),
-    });
+    public profileForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.profileForm = this.fb.group({
+            firstNameInput: [""],
+            lastNameInput: [""],
+            addressGroups: this.fb.array([
+                this.fb.group({
+                    streetInput: [""],
+                    cityInput: [""],
+                    stateInput: [""],
+                    zipCodeInput: [""],
+                }),
+            ]),
+        });
+    }
     
     public addAddressGroup() {
         const fa = this.profileForm.controls["addressGroups"] as FormArray;
         
         fa.push(this.newAddressGroup());
+    }
+
+    public saveProfileForm() {
+        console.log(this.profileForm.value);
     }
     
     private newAddressGroup() {
